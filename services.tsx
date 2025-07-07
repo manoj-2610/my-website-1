01796111c9ffe588db906d83ce9b44238c1f8187
manoj-2610 <rmanojprasad97@gmail.com>
2025-07-07 16:59:33 +0530

    add this in pages/services.tsx

---
+++ services.tsx
@@ -0,0 +1,15 @@
+const Services = () => {
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl font-bold mb-4">Our Legal Services</h1>
+      <ul className="list-disc list-inside space-y-2">
+        <li>Startup Incorporation</li>
+        <li>Contract Drafting</li>
+        <li>IP Registration</li>
+        <li>Due Diligence</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Services;
